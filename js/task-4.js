a2ff1973e5146296265651e574915931ec34f2f3
Sergey Shulga <shul.sergio@gmail.com>
2024-06-08 23:16:50 +0300

    v.1.5 add / task 4 done
    v.1.5 add / task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", hendlerClickOk);
 
+function hendlerClickOk(evt) {
+  evt.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value.trim();
+  });
+
+  if (data.email === "" || data.password === "") {
+    alert("All form fields must be filled in");
+  }
+  console.log(data);
+}
