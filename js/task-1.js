afff4135411beaa100f61c011b48c0799a679d1c
Sergey Shulga <shul.sergio@gmail.com>
2024-06-08 18:41:22 +0300

    v.1.3 add 1 task done
    v.1.3 add 1 task done

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,9 @@
 const checkLi = document.querySelectorAll(".item");
 console.log(`Number of categories: ${checkLi.length}`);
-
 const allLi = document.querySelectorAll(".item");
-items.forEach((item) => {
+allLi.forEach((item) => {
   const header = item.querySelector("h2").textContent;
   const itemLi = item.querySelectorAll("ul li");
-  console.log(`Категория: ${title}`);
+  console.log(`Категория: ${header}`);
   console.log(`Количество элементов: ${itemLi.length}`);
 });
--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,18 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+const gallery = document.querySelector(".gallery");
+
+function addHtmlMarckup() {
+  return images
+    .map(({ url, alt }) => {
+      return `<li>
+          <img src=${url} alt=${alt} />
+          </li>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("afterbegin", addHtmlMarckup());
