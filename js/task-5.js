12f97890fe315ec5f7ce2448c9d04331f0ec09b6
Sergey Shulga <shul.sergio@gmail.com>
2024-06-09 14:18:46 +0300

    v.1.7 add css 4all / task 5 done
    v.1.7
    change css in all task (main content in tasks
    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+import { getRandomHexColor } from "./task-6.js";
+
+let changeColorButton = document.querySelector(".change-color");
+let colorSpan = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", hendlerClickColorBtn);
+
+function hendlerClickColorBtn() {
+  let colorInput = getRandomHexColor();
+  document.body.style.backgroundColor = colorInput;
+  colorSpan.textContent = colorInput;
+  console.log("colorInput", colorInput);
+  console.log("colorSpan ", colorSpan);
 }
