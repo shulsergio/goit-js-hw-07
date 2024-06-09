fd6004ab3b3032203f6e5828d59476233d2dc918
Sergey Shulga <shul.sergio@gmail.com>
2024-06-09 15:51:03 +0300

    v.1.8 add css task 6
    v.1.8 add css task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,14 @@ export function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+let boxes = document.querySelector("#boxes");
+let amount = document.querySelector("input");
+let buttonCreate = document.querySelector("[data-create]");
+let buttonDestroy = document.querySelector("[data-destroy]");
+
+buttonCreate.addEventListener("click", () => {
+  createBoxes(amount);
+});
+function createBoxes(amount) {
+  console.log("amount.value-", amount.value);
+}
